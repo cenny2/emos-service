b132b6d848ae7b0cd7a0b7752dd584effe24d917
Wuc <hta1320@163.com>
2022-07-14 21:34:03 +0800

    proj init..

---
+++ src/main/java/com/ht/emos/EmosServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ht.emos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmosServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmosServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ht/emos/EmosServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ht.emos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmosServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
